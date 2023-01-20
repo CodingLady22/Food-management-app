e643a54cc8cd43ca4d2b54d4ad587cde0017f61f
Maye Jesuorobo <nayawes@yahoo.com>
2023-01-20 16:41:38 +0100

    Created a model for the comments which includes a ref to the user model

---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+    title : {
+        type: String,
+        require: true
+    },
+    comment : {
+        type: String,
+        require: true
+    },
+    User : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }
+})
+
+module.exports = mongoose.model('Comment', commentSchema)
