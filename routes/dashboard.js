41dd1b13e7e34edf8cdbba1592159c924e403133
Maye Jesuorobo <nayawes@yahoo.com>
2023-02-14 01:15:56 +0100

    edited the file to remove the routes for the comments file. Left the createComment route for now

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,7 +6,7 @@ const commentsController = require('../controllers/comments')
 // * Handles initial GET request from the homepage
 
 router.get('/', dashboardController.getIndex) // read
-router.get('/comment', commentsController.getComments) // read
+
 
 //* Handles POST method request for adding a new item
 
