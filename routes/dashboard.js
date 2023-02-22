45afc0bea2f9bf15a9373fb0bf4e45147d2ff978
Maye Jesuorobo <nayawes@yahoo.com>
2023-02-22 22:58:59 +0100

    added the routes to the edit page and to delete and update items

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,10 +6,13 @@ const commentsController = require('../controllers/comments')
 // * Handles initial GET request from the homepage
 
 router.get('/', dashboardController.getIndex) // read
+router.get('/edit/:id', dashboardController.editItems) // read
 
 
 //* Handles POST method request for adding a new item
 
 router.post('/dashboard', dashboardController.createItem) // create
+router.post('/update/:id', dashboardController.updateItem) // update
+router.get('/delete/:id', dashboardController.deleteItem) // delete - check if POST works or needs READ instead
 
 module.exports = router
