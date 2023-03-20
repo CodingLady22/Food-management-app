645a89949c31c05c02dd0cac1dc48b6eff1e4632
Maye Jesuorobo <nayawes@yahoo.com>
2023-03-20 22:12:03 +0100

    updated the path for the dashboard page to /dash

--- controllers/comments.js
+++ controllers/comments.js
@@ -22,7 +22,7 @@ module.exports = {
             res.redirect('/comments')
         } catch (err) {
             if (err) return res.status(500).send(err)
-            res.redirect('/')
+            res.redirect('/dash')
         }
     },
     // getting to the edit page
