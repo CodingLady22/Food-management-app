71c1aefce91fb5fe9e5bd28f0b89ce11b2e01ba1
Maye Jesuorobo <nayawes@yahoo.com>
2023-04-28 22:50:09 +0200

    uncommented the user property

--- models/NewItems.js
+++ models/NewItems.js
@@ -17,6 +17,10 @@ const newItemSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
     purchaseDate: {
         type: Date,
         default: Date.now
