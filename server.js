db31241912ee5cda92e372b399058c00eeea040f
Maye Jesuorobo <nayawes@yahoo.com>
2023-07-22 01:19:54 +0200

    added the methode override package to use on the recipe pages

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
 const dashboardRoutes = require("./routes/dashboard");
@@ -36,6 +37,9 @@ app.use(express.json());
 
 //Logging
 app.use(logger("dev"));
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
 
 // Setup Sessions - stored in MongoDB
 app.use(
