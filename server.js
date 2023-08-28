d62c9085887b086da48723d2d14468bbf65d67b1
Maye Jesuorobo <nayawes@yahoo.com>
2023-08-28 18:36:20 +0200

    added an alternative port 3000 for cyclic

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.use('/', homeRoutes)
 
 
 //todo - Start Server
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running on port ${process.env.PORT}`)
 });
