3ff771141f0bc938fa2d682201296b5265df3366
Maye Jesuorobo <nayawes@yahoo.com>
2023-09-15 03:11:37 +0200

    Refactor code comments for improved clarity and documentation

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,38 +1,89 @@
+/**
+ * Controller for managing user recipes, including creation, viewing, editing, deletion, and saving.
+ * Handles data retrieval, validation, and rendering of views.
+ *
+ * @module RecipesController
+ */
+
 const cloudinary = require("../middleware/cloudinary");
 const Recipe = require("../models/Recipe")
-const User = require("../models/User") //! This model was brought in ONLY for the displaySavedRecipes() because the saved recipes are stored in an array in the User database and I want all methods for the recipes to be in one, this recipe, file
+const User = require("../models/User") //! This model was brought in ONLY for the displaySavedRecipes() because the saved recipes are stored in an array in the User database and all methods for the recipes are to be in one file
 const axios = require("axios");
 // const UploadedRecipe = require("../models/UploadedRecipe")
 
 module.exports = {
+    /**
+    * Retrieves and renders the user's recipes by querying the database for recipes associated with the current user.
+    * Renders the 'recipes.ejs' view with the retrieved recipes and the user object for display.
+    *
+    * @function
+    * @async
+    * @param {Object} req - Express request object.
+    * @param {Object} res - Express response object.
+    * @returns {void}
+    */
     getRecipes: async (req, res) => {
         try {
+            // Query the database to find recipes associated with the current user
             const recipes = await Recipe.find({ user: req.user.id })
+
+            // Render the 'recipes.ejs' view, passing the retrieved recipes and the user object for display
             res.render('recipes.ejs', { getRecipes: recipes, user: req.user })
         } catch (err) {
+            // Handle errors and send an appropriate response with a 500 status code
             if (err) return res.status(500).send(err)
         }
     },
+    /**
+    * Retrieves and renders a feed of all recipes sorted by creation date, including quotes.
+    * Integrates with an external API to fetch random quotes.
+    *
+    * @function
+    * @async
+    * @param {Object} req - Express request object.
+    * @param {Object} res - Express response object.
+    * @returns {void}
+    */
     recipeFeed: async (req, res) => {
         try {
+            // Retrieve all recipes, sorted by creation date, and populate user information
             const recipes = await Recipe.find().sort({ createdAt: "desc" }).populate("user");
+
+            // Fetch random quotes from an external API
             const response = await axios.get('https://lazy-ox-trunks.cyclic.cloud/api/next-quote');
             const quotes = response.data;
 
+             // Render the allRecipes view with recipe and quote data
             res.render('allRecipes.ejs', { recipes: recipes, quotes: quotes, user: req.user })
         } catch (err) {
+            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err)
         }
     },
+    /**
+    * Displays the saved recipes for the current user by fetching and rendering saved recipes from the database.
+    * Retrieves quotes from an external API for display.
+    *
+    * @function
+    * @async
+    * @param {Object} req - Express request object.
+    * @param {Object} res - Express response object.
+    * @returns {void}
+    */
     displaySavedRecipes: async (req, res) => {
         try {
+            // Retrieve the current user's saved recipes from the database, also populating the user details for each saved recipe
             const user = await User.findById(req.user.id)
             .populate({ path: 'savedRecipes', populate: { path: 'user' } });
 
+            // Fetch quotes from an external API for additional content
             const response = await axios.get('https://lazy-ox-trunks.cyclic.cloud/api/next-quote');
             const quotes = response.data;
+
+             // Render the 'savedRecipes.ejs' view with the retrieved saved recipes, quotes, and user object for display
             res.render('savedRecipes.ejs', { savedRecipes: user.savedRecipes, quotes: quotes, user: req.user })
         } catch (err) {
+            // Handle errors, log them, and send an appropriate response with a 500 status code
             console.error('Error fetching saved recipes:', err);
             res.status(500).send('Error fetching saved recipes');
         }
@@ -47,6 +98,7 @@ module.exports = {
             await User.findByIdAndUpdate(userId, { $addToSet: { savedRecipes: recipeId } });
             res.redirect('/recipes/viewRecipeFeed'); // Redirect the user back to the recipes page after saving
         } catch (err) {
+            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err);
         }
     },
@@ -68,19 +120,31 @@ module.exports = {
         //     res.redirect('/')
         // }
     },
+    /**
+    * Creates a new recipe by processing user-submitted form data, including image upload to Cloudinary.
+    * Converts and stores hashtags as an array of strings.
+    *
+    * @function
+    * @async
+    * @param {Object} req - Express request object.
+    * @param {Object} res - Express response object.
+    * @returns {void}
+    */
     createRecipe: async (req, res) => {
         // Upload image to cloudinary
         const result = await cloudinary.uploader.upload(req.file.path);
 
-        // Convert hashtag string to an array of strings
+        // Initialize an empty array to store hashtags
         let hashtagsArray = [];
 
+        // Check if hashtags are provided in the request body
         if (req.body.hashtags) {
+            // Check if hashtags are comma-separated
             if (req.body.hashtags.includes(',')) {
-            // Handle comma-separated hashtags
+            // Split, trim, and join the comma-separated hashtags into an array
             hashtagsArray = req.body.hashtags.split(',').map(tag => tag.trim()).join(' ');
             } else {
-            // Handle space-separated hashtags
+            // Split, trim, and join space-separated hashtags into an array
             hashtagsArray = req.body.hashtags.split(' ').filter(tag => tag.trim() !== '').join(' ');
             }
         }
@@ -90,6 +154,7 @@ module.exports = {
         //     hashtagsArray = req.body.hashtags.split(/[, ]+/).filter(tag => tag.trim() !== '').join(' ');
         // }
       
+        // Create a new Recipe document with the processed data
         const newRecipe = new Recipe(
             {
                 title: req.body.title,
@@ -107,80 +172,80 @@ module.exports = {
             }
         )
         try {
+            // Save the new recipe to the database
             await newRecipe.save()
-            console.log(newRecipe)
+            // console.log(newRecipe)
+            // Redirect the user to the recipes page
             res.redirect('/recipes')
         } catch (err) {
+            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err)
             res.redirect('/')
         }
     },
+    /**
+ * Displays a single recipe for viewing, including options to edit if specified.
+ * Retrieves and renders details of a single recipe by its unique identifier, populating user details.
+ *
+ * @function
+ * @async
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {void}
+ */
     singleRecipe: async (req, res) => {
         try {
+            // Retrieve details of a single recipe by its unique identifier, and populate user details for display
             const viewRecipe = await Recipe.findById(req.params.id).populate("user");
+
+            // Check if the query parameter 'edit' is set to 'true' to determine whether to show the edit form
             const showEditForm = req.query.edit === 'true';
+
+            // Render the 'oneRecipe.ejs' view with the retrieved recipe details, user object, and showEditForm option
             res.render('oneRecipe.ejs', { recipe: viewRecipe, user: req.user, showEditForm: showEditForm })
         } catch (err) {
+            // Handle errors
             console.log(err);
         }
     },
-    // ! Had a seperate method to update the image before putting all updates in one method
-    /*
-    updateRecipeImage: async (req, res) => {
-        const id = req.params.id;
-        try {
-            // change name of this variable
-            const updatedRecipe = await Recipe.findById(id);
-            
-            // first check if a new image is being uploaded
-            if(req.file) {
-                // first upload the new image
-                const newImageUpload = await cloudinary.uploader.upload(req.file.path);
-                const newImagePublicId = newImageUpload.public_id;
-
-                // next, retrieve the existing image details from the database
-                const oldImagePublicId = updatedRecipe.cloudinaryId;
-
-                // then delete the old image from cloudinary
-                await cloudinary.uploader.destroy(oldImagePublicId);
-
-                // lastly, update the image URL and cloudinary id in the database
-                updatedRecipe.image = newImageUpload.secure_url;
-                updatedRecipe.cloudinaryId = newImagePublicId;
-            }
-            await updatedRecipe.save();
-            console.log("Image has been updated")
-            res.redirect(`/recipes/viewRecipe/${id}`)
-            
-        } catch (err) {
-            res.status(500).send('Error retrieving image for editing.')
-        }
-    },
+    /**
+    * Updates a recipe's details, including the option to upload a new image.
+    * Retrieves the recipe by its unique identifier, updates its properties with the provided data,
+    * and handles the uploading of a new image if specified.
+    *
+    * @function
+    * @async
+    * @param {Object} req - Express request object.
+    * @param {Object} res - Express response object.
+    * @returns {void}
     */
     updateRecipe: async (req, res) => {
         const id = req.params.id;
         try {
+            // Destructure recipe details from the request body
             const { title, prep, cook, total, serving, ingredients, instructions, note, hashtags } = req.body;
+
+             // Retrieve the existing recipe by its unique identifier
             const updatedRecipe = await Recipe.findById(id);
 
-            // first check if a new image is being uploaded
+            // Check if a new image is being uploaded
             if(req.file) {
-                // first upload the new image
+                // Upload the new image to Cloudinary
                 const newImageUpload = await cloudinary.uploader.upload(req.file.path);
                 const newImagePublicId = newImageUpload.public_id;
 
-                // next, retrieve the existing image details from the database
+                // Retrieve the existing image's public ID from the database
                 const oldImagePublicId = updatedRecipe.cloudinaryId;
 
-                // then delete the old image from cloudinary
+                // Delete the old image from Cloudinary
                 await cloudinary.uploader.destroy(oldImagePublicId);
 
-                // lastly, update the image URL and cloudinary id in the database
+                // Update the image URL and Cloudinary ID in the database
                 updatedRecipe.image = newImageUpload.secure_url;
                 updatedRecipe.cloudinaryId = newImagePublicId;
             }
 
-            // now update the other recipe details in the database
+            // Update the recipe's properties with the provided data
             await Recipe.findByIdAndUpdate(updatedRecipe, {
                 title,
                 prep,
@@ -196,9 +261,13 @@ module.exports = {
         //    save the updated recipe in the database
         await updatedRecipe.save();
         
+        // Log a success message
             console.log("Recipe has been updated")
+            
+            // Redirect to the recipes view page after updating
             res.redirect(`/recipes/viewRecipe/${id}`)
         } catch (err) {
+            // Handle errors and send an appropriate response
             res.status(500).send('Error retrieving recipe for editing.')
         }
     },
@@ -227,17 +296,18 @@ module.exports = {
         //   Find post by id
         const id = req.params.id
         try {
-            // let recipe = await Recipe.findById({ id: req.params.id });
+        // Find and delete the user's individual recipe based on its unique identifier.
         let recipe = await Recipe.findByIdAndDelete(id);
 
       // Delete image from cloudinary
       await cloudinary.uploader.destroy(recipe.cloudinaryId);
-      // Delete post from db
-    //   await Recipe.remove({ id: req.params.id });
+      // Delete post from db based on its unique identifier.
       await Recipe.findByIdAndDelete(id);
       console.log("Recipe has been deleted");
+      // Redirect to the recipes page after deletion
             res.redirect('/recipes')
     } catch (err) {
+        // Handle errors and send an appropriate response
       err.status(500).send(err)
       res.redirect('/recipes')
     }
