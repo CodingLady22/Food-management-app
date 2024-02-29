d2bb57a7782cbbac5af7e86c85bdadf46f16d8e0
Maye Jesuorobo <nayawes@yahoo.com>
2024-02-29 19:54:18 +0100

    center the 'save' button in oneRecipe.ejs and clean out comments in other files. Change postcss and tailwind config files from .js to .cjs after cloning app

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,20 +1,19 @@
 import Comment from '../models/Comment.js';
 
 
+// * Display user comments
     export const getComments = async (req, res) => {
         try {
-            console.log(req.user.id);
-            // Retrieve user's comments and notes 
             const comments = await Comment.find({ user: req.user.id })
-            // Render the comments view with data
+            
             res.render('comments.ejs', { enteredComments: comments, user: req.user })
         } catch (err) {
             if (err) return res.status(500).send(err)
         }
     }
     
+    // * Create new comments
     export const createComment = async (req, res) => {
-        // Creates a new user comment or note containing the comment's title and content and saves it to the database.
         const newComment = new Comment(
             {
                 title: req.body.title,
@@ -23,72 +22,61 @@ import Comment from '../models/Comment.js';
             }
         )
         try {
-            // Save the newly created user comment or note to the database
             await newComment.save()
-            // console.log(newComment)
-             // Redirect to the comments and notes page after successful creation
+
             res.redirect('/comments')
         } catch (err) {
-            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err)
-            // Redirect to an error page or dashboard in case of an error
             res.redirect('/dash')
         }
     }
 
-    // Retrieves and renders an individual user comment or note in full detail.
-    // This function is used to view the complete content of a specific entry.
+    // * Retrieves and renders an individual user comment or note in full.
     export const getOneComment = async (req, res) => {
         try {
-            console.log(req.user.id);
-            // Retrieve a single user comment or note by its unique identifier
             const comments = await Comment.findById(req.params.id)
-             // Render the viewComment view to display the full content of the entry
+            
             res.render('viewComment.ejs', { oneComment: comments, user: req.user })
         } catch (err) {
             if (err) return res.status(500).send(err)
         }
     }
-    // Function to get edit page
+
+    //* Display edit page
     export const editComments = async (req, res) => {
         const id = req.params.id
-        console.log(id);
         try {
             const comments = await Comment.find()
-            // Render the editComment view with data
             res.render('editComment.ejs', { editComment : comments, commentId : id})
         } catch (err) {
             if (err) return res.status(500).send(err)
         }
     }
-    // Function to update notes in comments
+
+    // * Update comments
     export const updateComment = async (req, res) => {
         const id = req.params.id
         try {
-            // Updates the user's individual comment or note based on its unique identifier.
             await Comment.findByIdAndUpdate(id, {
                 title: req.body.title,
                 comment: req.body.comment
             })
-            // Redirect to the comments and notes page after updating
+            
             res.redirect('/comments')
         } catch (err) {
-            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err)
             res.redirect('/comments')
         }
     }
     
+    // * Delete comment
     export const deleteComment = async (req, res) => {
         const id = req.params.id
         try {
-            // Delete the user's individual comment or note based on its unique identifier.
             const comment = await Comment.findByIdAndDelete(id)
-            console.log(comment);
-            // Redirect to the comments and notes page after deletion
+            console.log(`${comment} has been deleted`);
             res.redirect('/comments')
         } catch (err) {
-            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err)
         }
     }
--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -12,15 +12,13 @@ import NewItem from "../models/NewItems.js";
         const purchaseDateError = req.flash('purchaseDateError');
         const expiryDateError = req.flash('expiryDateError');
         try {
-            // Retrieve user's items
+            
             console.log(req.user.id);
             const items = await NewItem.find({ user: req.user.id })
 
-            // Initialize arrays to store days left for each item and count of expiring items
             let daysLeft = [];
             let expiringSoonCount = 0;
 
-            // Calculate days left for each item and check if it's expiring soon
             for(let i = 0; i < items.length; i++) {
                 let expirations = new Date(items[i].expiry)
                 let today = new Date();
@@ -36,19 +34,16 @@ import NewItem from "../models/NewItems.js";
                     }
                 }
             }
-            // console.log(daysLeft);
-            // Render the dashboard view with data
+
             res.render('dashboard.ejs', {newItem: items, user: req.user, daysLeft: daysLeft, expiringSoonCount: expiringSoonCount, purchaseDateError: purchaseDateError, expiryDateError: expiryDateError})
         } catch (err) {
-            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err)
         }
     }
 
-    //* 
+    //* Create newly entered food item in dashboard
     export const createItem = async (req, res) => {
 
-        // Creates user's items
         const newItem = new NewItem(
             {
                 itemInput: req.body.itemInput,
@@ -60,23 +55,20 @@ import NewItem from "../models/NewItems.js";
             }
         );
         
-        // Date validation 
         const purchaseDate = new Date(req.body.purchaseDate);
         const expiryDate = new Date(req.body.expiry);
         const currentDate = new Date();
 
         if(purchaseDate > currentDate) {
-            // Check if date of purchase is in the future
+
             req.flash('purchaseDateError', 'Purchase date cannot be in the future.')
             return res.redirect('/dash')
         }
         if (expiryDate < currentDate) {
-            // Check if date of expiration is in the past
             req.flash('expiryDateError', 'Expiry date cannot be in the past.')
             return res.redirect('/dash')
         }
         if (expiryDate < purchaseDate) {
-            // Check if date of expiration is before date of purchase
             req.flash('expiryDateError', 'Expiry date cannot be before the purchase date.')
             return res.redirect('/dash')
         }
@@ -84,73 +76,64 @@ import NewItem from "../models/NewItems.js";
         try {
             await newItem.save()
             console.log(newItem)
-            // Redirect to dashboard view
+            
             res.redirect('/dash')
         } catch (err) {
-            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err)
             res.redirect('/dash')
         }
     }
 
+    // * Display only items expring within 7 days
     export const getExpiringItems = async (req, res) => {
         try {
-            // Retrieve user's items
             const items = await NewItem.find({ user: req.user.id })
 
-            // Initialize arrays to store days left for each item and count of expiring items
             let expiringItems = [];
             let expiringDaysLeft = [];
 
-            // Calculate days left for each item
             for (let i = 0; i < items.length; i++) {
                 let expiration = new Date(items[i].expiry);
                 let today = new Date();
                 let timeDiff = expiration.getTime() - today.getTime();
 
             if (timeDiff > 0 && timeDiff <= 7 * 24 * 60 * 60 * 1000) {
-                // Item expiring within 7 days
                 expiringItems.push(items[i]);
                 let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
                 expiringDaysLeft.push(diffDays);
             }
         }
 
-        // Render the expirationPage view with data
         res.render('expirationPage.ejs', {
             user: req.user,
             expiringItems: expiringItems,
             expiringDaysLeft: expiringDaysLeft
         });
         } catch (err) {
-            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err);
         }
     }
 
-      // Function to get edit page
+      //* Display edit page
+      
     export const editItems = async (req, res) => {
         const id = req.params.id
-        // Error messages for wrong date inputs
         const purchaseDateError = req.flash('purchaseDateError');
         const expiryDateError = req.flash('expiryDateError');
         try {
             const items = await NewItem.find()
-            // Render the editDashboard view with data
             res.render('editDashboard.ejs', { dashItems : items, dashId : id, purchaseDateError: purchaseDateError, expiryDateError: expiryDateError})
         } catch (err) {
-            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err)
         }
     }
 
-    // Function to update items in dashboard
+    //* Update items in dashboard
     export const updateItem = async (req, res) => {
         const id = req.params.id
 
         try {
             console.log('Item has been updated');
-            // Retrieve and updates user's item based on its unique identifier.
             await NewItem.findByIdAndUpdate(id, {
                 itemInput: req.body.itemInput,
                 quantity: req.body.quantity,
@@ -160,46 +143,39 @@ import NewItem from "../models/NewItems.js";
                 user: req.user.id,
             })
 
-            // Date validation 
         const purchaseDate = new Date(req.body.purchaseDate);
         const expiryDate = new Date(req.body.expiry);
         const currentDate = new Date();
 
         if(purchaseDate > currentDate) {
-            // Check if date of purchase is in the future
             req.flash('purchaseDateError', 'Purchase date cannot be in the future.')
             return res.redirect(`/dash/edit/${id}`)
         }
         if (expiryDate < currentDate) {
-            // Check if date of expiration is in the past
             req.flash('expiryDateError', 'Expiry date cannot be in the past.')
             return res.redirect(`/dash/edit/${id}`)
         }
         if (expiryDate < purchaseDate) {
-            // Check if date of expiration is before date of purchase
             req.flash('expiryDateError', 'Expiry date cannot be before the purchase date.')
             return res.redirect(`/dash/edit/${id}`)
         }
         
-        // Redirect to dashboard view
             res.redirect('/dash')
         } catch (err) {
-            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err)
             res.redirect('/dash')
         }
     }
 
+    // * Delete items in dashboard
     export const deleteItem = async (req, res) => {
         const id = req.params.id
         try {
-            // Delete the user's individual item based on its unique identifier.
             const item = await NewItem.findByIdAndDelete(id)
             console.log(item);
-            // Redirects to dashboard view
+            
             res.redirect('/dash')
         } catch (err) {
-            // Handle errors and send an appropriate response
             if (err) return res.status(500).send(err)
         }
     }
